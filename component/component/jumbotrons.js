b0ea6bc71f2df90daea55b4e0e02a4b49831ff5e
FaradayJnr <faradafoundationgh@gmail.com>
2023-04-19 13:19:32 +0000

    Mission and vission buttons

--- component/component/jumbotrons.js
+++ component/component/jumbotrons.js
@@ -11,18 +11,19 @@ class Jumbotron extends HTMLElement {
     
     <div class="text-center bg-body-tertiary rounded-3" style="margin: 15px;font-size: 15px;">
     <div class="d-inline-flex gap-2">
-      <button class="d-inline-flex align-items-center btn btn-primary btn-sm rounded-pill" type="button">
+      <button id="missionBtn" class="d-inline-flex align-items-center btn btn-primary btn-sm rounded-pill active" type="button" onclick="displayMission()">
         <span class="fs-6">Our Mission</span>
       </button>
-      <button class="btn btn-outline-secondary btn-sm rounded-pill" type="button" onclick="document.getElementById('paragraph');">
+      <button id="visionBtn" class="btn btn-outline-secondary btn-sm rounded-pill" type="button" onclick="displayVision()">
         <span class="fs-6">Our Vision</span>
       </button>
     </div>
     <p id="paragraph" class="col-lg-8 mx-auto fs-7 text-muted"><i>
-    This is a custom jumbotron featuring an SVG image at the top, some longer text that wraps early thanks to a responsive <code>.col-*</code> class, and a customized call to action.
-  <i></p>
+      This is a custom jumbotron featuring an SVG image at the top, some longer text that wraps early thanks to a responsive <code>.col-*</code> class, and a customized call to action.
+    </i></p>
   </div>
   
+  </div>
         `;
   }
 }
